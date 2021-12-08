0ff0c977f9ddf6801c677baf2bdf8929a24ec5e9
VuHuyNhatMinh <nminhstickpk@gmail.com>
2021-12-08 09:59:55 +0700

    Delete comment

--- test.c
+++ test.c
@@ -19,11 +19,6 @@ typedef union {
 	struct
 	{
 
-		// Order is important.
-		// Here the members of the union data structure
-		// use the same memory (32 bits).
-		// The ordering is taken
-		// from the LSB to the MSB.
 		unsigned int mantissa : 23;
 		unsigned int exponent : 8;
 		unsigned int sign : 1;
