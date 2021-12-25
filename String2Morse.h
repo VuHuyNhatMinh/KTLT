1cd8bd8cad44ea39e0dc7b395c481634d2489147
VuHuyNhatMinh <nminhstickpk@gmail.com>
2021-12-26 06:50:06 +0700

    2612

--- String2Morse.h
+++ String2Morse.h
@@ -9,15 +9,22 @@ using namespace std;
 // Đầu ra: là một con trỏ tới vùng nhớ động được cấp phát có chứa đoạn mã Morse đã được dịch ra. (Cần xóa vùng bộ nhớ động được cấp phát sau khi dùng. )
 string* Textmorse(string* text){
     string* code = new string;
+    if (!code)
+    {
+        return NULL;
+    }
+    
     for (int i = 0; i < text->length(); i++)
     {
         if (text->at(i) != ' ')
         {
             *code += Char2Morse(text->at(i)) + ' ';
         }
-        else *code += '/';
-
-        *code += ' ';
+        else 
+        {
+            *code += '/';
+            *code += ' ';
+        }
     }
     return code;
 }
