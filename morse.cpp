f67bf57d71e56e59305e3ea3aa9bc8d2aa96d951
Vu Huy Nhat Minh <83871693+VuHuyNhatMinh@users.noreply.github.com>
2021-12-25 18:39:14 +0700

    Add files via upload

--- morse.cpp
+++ morse.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
     Author: 
 */
@@ -62,4 +63,70 @@ int main(int argc, char* argv[])
     }
     cout << res << endl;
     system("pause>0");
+=======
+/*
+    Author: 
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Dictionary.h"
+#include "ErrorChecking.h"
+#include "Morse2String.h"
+#include "String2Morse.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    /* 
+        Check whether open file data or not 
+    */
+    if (!check_open(argv[1]))
+    {
+        return 1;
+    }
+
+    /*
+        Check whether file result exsit or not 
+        If does, overwrite or not
+    */
+    if (!check_exist_out(argv[2]))
+    {
+        return 1;
+    }
+
+    // Open file data to perform decrypt
+    ifstream fdata;
+    fdata.open(argv[1], ios::in);
+    // An object string data to store all text in data file
+    string data;    
+    getline(fdata, data, '\0');
+    fdata.close();
+   
+    /*
+        Check the file type
+    */
+    // An object string res to store decryption data
+    string res;
+    if (check_type(data))
+    {
+        // If it is morse file
+        string *input = &data;
+        string* english = decrypt(input);
+        cout << *english << '\n';
+
+    }
+    else
+    {
+        // If it is text file
+        // check_text(data);
+
+        // Decrypt to morse file
+        res = Textmorse(data);
+    }
+    cout << res << endl;
+    system("pause>0");
+>>>>>>> 629e16b67dcab567b0c05d4748845f5ec6a7f7ce
 }
