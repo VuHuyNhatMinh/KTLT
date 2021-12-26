0d9e6dc7c18e99f5208695c47271706967150915
linhlamcau2 <95729896+linhlamcau2@users.noreply.github.com>
2021-12-26 16:03:39 +0700

    Add files via upload
    sửa đổi

--- String2morse.h
+++ String2morse.h
@@ -1,11 +1,17 @@
+/*
+  Author: NguyenTienLinh-20191924
+*/ 
+
 #include <iostream>
 #include <string>
 #include"Dictionary.h"
 using namespace std;
+// Hàm dùng để chuyển chuỗi text hợp lệ sang đoạn mã Morse tuong ứng.
+// Đầu vào là  chuỗi string được copy ra từ file text. 
+// Đầu ra: là  đoạn mã Morse tương ứng 
  string Textmorse(string text){
      string code = "";
-     // Input : chu
-     // Output: ma morse
+     
 
      for (int i = 0; i < text.length(); i++)
      {
