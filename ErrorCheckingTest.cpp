be5a55cb6291718a1e5ff83ce08a7aa859f9f9de
Quân Đinh <you@example.com>
2021-12-26 13:27:45 +0700

    them ki tu xuong dong

--- ErrorCheckingTest.cpp
+++ ErrorCheckingTest.cpp
@@ -26,7 +26,7 @@ bool check_type(string a)       //input là một string
 {                               //output là true nếu là morse, là false nếu là text 
     for( int i=0;i<a.size();i++)
     {
-        if(a[i]!='.'&& a[i]!='-' && a[i]!='/'&&a[i]!=' ')
+        if(a[i]!='.'&& a[i]!='-' && a[i]!='/'&&a[i]!=' '&&a[i]!='\n')
         {
             return false;
         }
