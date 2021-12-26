6dc2d310d5b38f7219379a1a073a43fa46c65509
Dai Nguyen <37876425+daindq@users.noreply.github.com>
2021-12-26 15:50:40 +0700

    2612

--- Morse2String.h
+++ Morse2String.h
@@ -32,3 +32,25 @@ string* decrypt(string* input){
     }
     return output;
 }
+
+// Decrypt no dynamic mem
+string Decrypt(string input){
+    input += " "; //extra space added at the end to access the last morse code
+    string output ; 
+    string temp;
+    for (int i = 0; i < input.length(); i++)
+    {
+        if (input.at(i) != ' ' && input[i] != '/' && input[i] != '\n'){ // Check space, "new line" (\n) and slash.
+            temp += input[i]; //storing morse code of a single character
+        }
+        else if (input[i] == '/'){//in case of forward-slash
+            output += ' '; // append ' ' to the output string.
+        }        
+        else if(input[i] == ' ' || input[i] == '\n'){ //in case of space or '\n'       
+            output += Morse2Char(temp); // search the dictionary for matching character.
+            temp.clear(); // clear the temporary variable.
+            if(input[i] == '\n'){output += '\n';} // in case of '\n' append '\n' to output.
+        }
+    }
+    return output;
+}
